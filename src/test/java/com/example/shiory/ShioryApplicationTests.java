99f7011a01a1df74815f7e860c12ec6d8e2b1eff
Hina263 <hinato263@gmail.com>
2026-08-21 23:41:51 +0900

    Spring Bootの共通環境を構築

---
+++ src/test/java/com/example/shiory/ShioryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShioryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
